092166deb144fcd56ba25cb822b662bea9b16ed6
jsakamotoIGJP <jsakamoto@infragistics.com>
2023-02-02 15:13:01 +0900

    初回コミット

---
+++ Data/Person.cs
@@ -0,0 +1,14 @@
+﻿namespace KB9905_IgbGridRowColors_BlazorServerApp1.Data;
+
+public class Person
+{
+    public int ID { get; set; }
+    public String FamilyName { get; set; } = String.Empty;
+    public String GivenName { get; set; } = String.Empty;
+    public String Prefecture { get; set; } = String.Empty;
+    public String City { get; set; } = String.Empty;
+
+    public Person()
+    {
+    }
+}
